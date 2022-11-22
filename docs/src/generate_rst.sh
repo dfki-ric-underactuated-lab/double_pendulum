b35ea26434f5297b4562ce42aef7ba5b3d3656be
Felix Wiebe <felix.wiebe@dfki.de>
2022-11-22 18:07:14 +0100

    in sphinx autogenerated documentation set maxdepth of contents to 1 with sed on generated rst files

--- docs/src/generate_rst.sh
+++ docs/src/generate_rst.sh
@@ -1,1 +1,7 @@
 SPHINX_APIDOC_OPTIONS=members,undoc-members,show-inheritance sphinx-apidoc -o ../text/generated/ ../../src/python/double_pendulum/ -f -M -T
+# sphinx-apidoc doesn't allow setting maxdepth on subpackages
+sed -i "s/:maxdepth: 4/:maxdepth: 1/g" ../text/generated/*
+# sphinx toctree is very indentation sensitive, make it uniform
+sed -i "s/    /   /g" ../text/generated/*
+sed -i "s/ package//" ../text/generated/*
+sed -i "s/ module//" ../text/generated/*
