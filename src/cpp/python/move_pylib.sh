ac80d646ed1f72c4d59670e1a9967ef2a480afcb
Felix Wiebe <felix.wiebe@dfki.de>
2023-07-19 18:12:30 +0200

    cpp user installation: create site-packages directory if not existing

--- src/cpp/python/move_pylib.sh
+++ src/cpp/python/move_pylib.sh
@@ -1,5 +1,6 @@
 if [ $(python3 -c 'import site; print(site.ENABLE_USER_SITE)') == True ]
 then
+    mkdir -p $(python3 -c 'import site; print(site.USER_SITE+"/")')
     mv cppilqr.cpython-*.so $(python3 -c 'import site; print(site.USER_SITE+"/")')
 else
     mv cppilqr.cpython-*.so $(python3 -c 'import site; print(site.getsitepackages()[0]+"/")')
