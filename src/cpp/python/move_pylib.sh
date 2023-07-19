104dc195a0b76b0e9cb96ab8876906e5526e33ca
Felix Wiebe <felix.wiebe@dfki.de>
2023-07-19 12:26:48 +0200

    move c++ python bindings to user site-packages if available

--- src/cpp/python/move_pylib.sh
+++ src/cpp/python/move_pylib.sh
@@ -1,1 +1,6 @@
-mv cppilqr.cpython-*.so $(python3 -c 'import site; print(site.getsitepackages()[0]+"/")')
+if [ $(python3 -c 'import site; print(site.ENABLE_USER_SITE)') == True ]
+then
+    mv cppilqr.cpython-*.so $(python3 -c 'import site; print(site.USER_SITE+"/")')
+else
+    mv cppilqr.cpython-*.so $(python3 -c 'import site; print(site.getsitepackages()[0]+"/")')
+fi
