Fix the precision loss issue in gym_env.d_p_dynamics_func

src/python/double_pendulum/simulation/gym_env.py
@@ -15,7 +15,7 @@ class CustomEnv(gym.Env):
         ),
         act_space=gym.spaces.Box(np.array([-1.0, -1.0]), np.array([1.0, 1.0])),
         max_episode_steps=1000,
-        scaling = True
+        scaling=True,
     ):
         self.dynamics_func = dynamics_func
         self.reward_func = reward_func
@@ -31,7 +31,9 @@ class CustomEnv(gym.Env):
         self.scaling = scaling
 
     def step(self, action):
-        self.observation = self.dynamics_func(self.observation, action, scaling = self.scaling)
+        self.observation = self.dynamics_func(
+            self.observation, action, scaling=self.scaling
+        )
         reward = self.reward_func(self.observation, action)
         terminated = self.terminated_func(self.observation)
         info = {}
@@ -63,7 +65,7 @@ class double_pendulum_dynamics_func:
         state_representation=2,
         max_velocity=20.0,
         torque_limit=[5.0, 5.0],
-        scaling = True
+        scaling=True,
     ):
         self.simulator = simulator
         self.dt = dt
@@ -75,18 +77,17 @@ class double_pendulum_dynamics_func:
         self.torque_limit = torque_limit
         self.scaling = scaling
 
-    def __call__(self, state, action,scaling = True):
+    def __call__(self, state, action, scaling=True):
         if scaling:
             x = self.unscale_state(state)
             u = self.unscale_action(action)
             xn = self.integration(x, u)
             obs = self.normalize_state(xn)
-            return np.array(obs, dtype=np.float32)
+            return obs.copy()
         else:
             u = self.unscale_action(action)
             xn = self.integration(state, u)
-            return np.array(xn, dtype=np.float32)
-
+            return xn.copy()
 
     def integration(self, x, u):
         if self.integrator == "runge_kutta":