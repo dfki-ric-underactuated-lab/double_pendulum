222adbbde1fef649a997245803dc00f4a866d865
Tim Faust <faust.tim@web.de>
2024-10-02 14:00:20 +0200

    new policy

--- experiments/design_C.1/history_sac.py
+++ experiments/design_C.1/history_sac.py
@@ -5,8 +5,8 @@ from double_pendulum.controller.history_sac import HistorySACController
 
 if __name__ == '__main__':
 
-    env_type = "acrobot"
-    model = "final2_only2"
+    env_type = "pendubot"
+    model = "eval1"
     controller = HistorySACController(env_type, model_path="../../data/policies/design_C.1/model_1.1/" + env_type + "/history_sac/" + model)
     # controller.set_friction_compensation(damping=[0.001, 0.001], coulomb_fric=[0.16, 0.12])
     controller.init()
@@ -20,6 +20,6 @@ if __name__ == '__main__':
         motor_ids=[3, 1],
         tau_limit=[6, 6],
         motor_directions=[1.0, -1.0],
-        save_dir=os.path.join("data/" + env_type + "/data_tuesday2/" + model)
+        save_dir=os.path.join("data/data_wednesday/" + env_type + "/" + model)
     )
 
