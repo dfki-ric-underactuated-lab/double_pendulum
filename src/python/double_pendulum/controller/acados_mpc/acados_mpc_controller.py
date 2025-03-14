9725a91c2ffcd75e1c24879fbfced27f1edc3bdb
Maranderine <104585818+Maranderine@users.noreply.github.com>
2025-03-14 10:29:22 +0100

    Update acados_mpc_controller.py

--- src/python/double_pendulum/controller/acados_mpc/acados_mpc_controller.py
+++ src/python/double_pendulum/controller/acados_mpc/acados_mpc_controller.py
@@ -12,7 +12,7 @@ import concurrent.futures
 import sys
 import numpy as np
 import pandas as pd
-from double_pendulum.controller.PointPID import PointPIDController
+from double_pendulum.controller.acados_mpc.acados_model.PoinPID import PointPIDController
 
 """
 File containts:
