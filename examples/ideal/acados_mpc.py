9fe8997cb193afa3b5f205af69d7a00e40a62948
Blanka Burchard <blbu01@dfki.de>
2025-03-24 17:08:40 +0100

    update example/ideal/acados_mpc.py

--- examples/ideal/acados_mpc.py
+++ examples/ideal/acados_mpc.py
@@ -10,84 +10,86 @@ from double_pendulum.simulation.simulation import Simulator
 from double_pendulum.utils.plotting import plot_timeseries
 from double_pendulum.utils.csv_trajectory import save_trajectory, load_trajectory
 
-from double_pendulum.controller.acados_mpc.acados_mpc_controller import AcadosMpcController
+from double_pendulum.controller.acados_mpc.acados_mpc import AcadosMpc
 
-## labels
-design = "design_C.0"
-model = "model_3.0"
-traj_model = "model_3.1"
-
-# model parameter
+actuated_joint = 0
 mass = [0.608, 0.630]
 length = [0.3, 0.2]
 com = [0.275, 0.166]
 damping = [0.081, 0.0]
 gravity = 9.81
 inertia = [0.09, 0.09]
-Fmax = 6    #torque limit
 actuated_joint=0    #0 = pendubot, 1 = acrobot
 gravity=9.81
-bend_the_rules=True
 cfric = [0.093, 0.186]
+Fmax = 6    #torque limit
+bend_the_rules=True
 
 torque_limit = [0.0, 0.0]
 torque_limit[actuated_joint] = Fmax
 if bend_the_rules:
     torque_limit[np.argmin(torque_limit)] = 0.5
 
-# simulation parameter
-dt = 0.01
-t_final = 10  # 5.985
-start = np.array([np.pi/6, 0, 0.0, 0.0])
-goal = np.array([np.pi, 0, 0.0, 0.0])
-
 # controller parameters
 N_horizon=20
-prediction_horizon=2.0
-Nlp_max_iter=200
+prediction_horizon=0.5
+Nlp_max_iter=100000000
 
 if actuated_joint == 1: #acrobot
-    Q_mat = 2*np.diag([100, 100, 10, 10])
-    Qf_mat = 2*np.diag([10000, 10000, 100, 100])
+    Q_mat = 2*np.diag([1000, 1000, 100, 100])
+    Qf_mat = 2*np.diag([100000, 100000, 100, 100])
     R_mat = 2*np.diag([0.0001, 0.0001])
 
 if actuated_joint == 0: #pendubot
-    Q_mat = 2*np.diag([100, 100, 10, 10])
-    Qf_mat = 2*np.diag([10000, 10000, 100, 100]) 
+    Q_mat = 2*np.diag([1000, 1000, 100, 100])
+    Qf_mat = 2*np.diag([100000, 100000, 100, 100]) 
     R_mat = 2*np.diag([0.0001, 0.0001])
 
 vmax = 30 #rad/s
-vf = 0
+vf = 0 # terminal velcoity constraint
 
-# create save directory
-timestamp = datetime.today().strftime("%Y%m%d-%H%M%S")
-robot = "pendubot" if actuated_joint == 0 else "acrobot"
-save_dir = os.path.join("data", design, model, robot, "ilqr", "mpc", timestamp)
-os.makedirs(save_dir)
+mpc_cycle_dt = 0.005 # inner loop
+outer_cycle_dt = 0.005 # outer loop
 
+max_solve_time = mpc_cycle_dt * 20
+
+bend_the_rules = True
+friction_compensation_mpc = True
+friction_compensation_added = False
+solver_type = "SQP_RTI" #Select here [SQP, SQP_RTI, or DDP]
+
+# simulation parameter
+dt = outer_cycle_dt
+t_final = 20  # 5.985
+start = np.array([0,0,0,0])
+goal = np.array([np.pi, 0, 0.0, 0.0])
+
+# simulation plant
 plant = SymbolicDoublePendulum(
-    mass=mass,
-    length=length,
-    com=com,
-    damping=damping,
-    gravity=gravity,
-    coulomb_fric=cfric,
-    inertia=inertia,
+    mass,
+    length , 
+    com , 
+    damping , 
+    gravity, 
+    cfric , 
+    inertia , 
     torque_limit=torque_limit
 )
 
 sim = Simulator(plant=plant)
 
-controller = AcadosMpcController(
-    mass=mass,
-    length=length, 
-    damping=damping, 
-    coulomb_fric=cfric,
-    inertia=inertia,
-    com=com,
-    gravity=gravity,
-    torque_limit=torque_limit,
-    cheating_on_inactive_joint= np.min(torque_limit)
+if not friction_compensation_mpc:
+    cfric = np.zeros_like(cfric)
+
+controller = AcadosMpc(
+    mass,
+    length , 
+    com , 
+    damping ,
+    cfric ,
+    gravity, 
+    inertia , 
+    torque_limit
 )
 
 controller.set_start(start)
@@ -96,26 +98,35 @@ controller.set_parameters(
     N_horizon=N_horizon,
     prediction_horizon=prediction_horizon,
     Nlp_max_iter=Nlp_max_iter,
-    solver_type="SQP",
-    wrap_angle=False
+    max_solve_time=max_solve_time,
+    solver_type=solver_type,
+    wrap_angle=False,
+    fallback_on_solver_fail=True,
+    pd_KP=0.0,
+    pd_KD=0.0,
+    pd_KI=0,
+    cheating_on_inactive_joint=bend_the_rules,
+    mpc_cycle_dt=mpc_cycle_dt,
+    outer_cycle_dt=outer_cycle_dt,
+    pd_tracking=False,
+    warm_start=True
 )
 
-#controller.set_velocity_constraints(v_max=vmax)
-#controller.set_velocity_constraints(v_final=vf)
+controller.set_velocity_constraints(v_max=vmax, v_final=vf)
 controller.set_cost_parameters(Q_mat=Q_mat, Qf_mat=Qf_mat, R_mat=R_mat)
-#controller.load_init_traj(csv_path=init_csv_path)
+if friction_compensation_added:
+    controller.set_friction_compensation(coulomb_fric=cfric)
 controller.init()
 
 T, X, U = sim.simulate_and_animate(t0=0.0, x0=start,
                                    tf=t_final, dt=dt, controller=controller,
                                    integrator="runge_kutta",
                                    plot_forecast=True,
-                                   scale=0.5)
+                                   scale=0.5,
+                                   save_video=False)
 
-#plot_timeseries(T, X, U, None,
- #               plot_energy=False,
-  #              pos_y_lines=[0.0, np.pi],
-   #             tau_y_lines=[-torque_limit[1], torque_limit[1]],
- #               scale=0.5)
-#
-controller.save(save_dir)
+plot_timeseries(T, X, U, None,
+               plot_energy=False,
+               pos_y_lines=[0.0, np.pi],
+               tau_y_lines=[-torque_limit[0], torque_limit[0],torque_limit[1],torque_limit[1]],
+               scale=0.5)
