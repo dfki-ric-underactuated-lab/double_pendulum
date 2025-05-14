0bc14bf13b5245990e962528cb5900cff75ba1e3
turcato-niccolo <niccolo.turcato@studenti.unipd.it>
2025-05-14 19:36:39 +0200

    2025 hardware stage controllers.

--- experiments/icra_2025/con_mcpilco_pendubot.py
+++ experiments/icra_2025/con_mcpilco_pendubot.py
@@ -4,7 +4,6 @@ from double_pendulum.model.model_parameters import model_parameters
 from double_pendulum.controller.mcpilco.mcpilco_controller import Controller_multi_policy_sum_of_gaussians_with_angles_numpy
 from parameters import goal, design, model
 
-
 robot = "pendubot"
 torque_limit = [6.0, 0.5]
 
@@ -18,7 +17,6 @@ leaderboard_config = {
     "username": "adk",
 }
 
-
 dt = 1 / 330.0  # trained on 33 Hz
 ctrl_rate = 10  # control is still 33Hz, but control loop is working at matching frequency
 
@@ -28,12 +26,13 @@ controlled_joint = [0, 1]
 policy_par_path = (
     "../../data/policies/" + design + "/" + model + "/" + robot + "/MC-PILCO/global_policy_" + robot + ".np"
 )
-
 file = open(policy_par_path, "rb")
 parameters = pkl.load(file)
 
 controller = Controller_multi_policy_sum_of_gaussians_with_angles_numpy(
-    parameters, ctrl_rate, torque_limit, n_dof, controlled_joint, active_pos_list=[[0, 1], [0, 1]], active_vel_list=[[2, 3], [2, 3]], wait_steps=0
+    parameters, ctrl_rate, torque_limit, n_dof, controlled_joint,
+    active_pos_list=[[0, 1], [0, 1]],
+    active_vel_list=[[2, 3], [2, 3]], wait_steps=0
 )
 controller.set_goal(goal)
 controller.init()
